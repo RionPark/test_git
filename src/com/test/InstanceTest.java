7283ab466f81ea9fc9a5a952610208cb3fa2e1d9
i7D <i7D@DESKTOP-CGMNAON>
2022-11-08 21:42:41 +0900

    커밋

--- src/com/test/InstanceTest.java
+++ src/com/test/InstanceTest.java
@@ -12,7 +12,7 @@ public class InstanceTest {
 		 * 모든 멤버 변수 멤버 함수 즉 어떤 클래스안에 있는 것들은
 		 * static이 아니면 반드시 메모리 생성(인스턴스화)을 해줘야 하기 때문임!!
 		 */
-		
+		//저런 태구는 그랬구나..
 		InstanceTest it = new InstanceTest();
 		System.out.println(it.name.length()); //오류 뜬다 null pointer exception
 	}
