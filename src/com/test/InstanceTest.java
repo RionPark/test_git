6f58ab69c8392d9df9ea00570e54673481107b7a
i7D <i7D@DESKTOP-CGMNAON>
2022-11-08 21:14:07 +0900

    자 이건 처음깃이랑 연동하는 설명을 하기 위한 테스트이다.

---
+++ src/com/test/InstanceTest.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class InstanceTest {
+	
+	private String name = "";
+
+	public static void main(String[] args) {
+		/*
+		 * JVM
+		 * main메소드는 자바로 만들어진 어떤 프로그램의 시작점!!
+		 * 
+		 * 모든 멤버 변수 멤버 함수 즉 어떤 클래스안에 있는 것들은
+		 * static이 아니면 반드시 메모리 생성(인스턴스화)을 해줘야 하기 때문임!!
+		 */
+		
+		InstanceTest it = new InstanceTest();
+		System.out.println(it.name.length()); //오류 뜬다 null pointer exception
+	}
+}
